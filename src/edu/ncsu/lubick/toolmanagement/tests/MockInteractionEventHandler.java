dc9b2ff3cb48c5dfafc17e4225f2fea2b973c59d
Kevin Lubick <kjlubick@users.noreply.github.com>
2013-10-21 14:56:48 -0400

    Added in another test to make sure the system handles WorkbenchWindow events well

--- src/edu/ncsu/lubick/toolmanagement/tests/MockInteractionEventHandler.java
+++ src/edu/ncsu/lubick/toolmanagement/tests/MockInteractionEventHandler.java
@@ -17,35 +17,48 @@ public class MockInteractionEventHandler
 	static final String MENU_NAME_OPEN_CALL_HIERARCHY = "OpenCallHierarchy";
 
 	//Keybindings
-	static final String MENU_KEYBINDING = "MENU";	//not a keybinding, but this is what the local hub will be expecting.
+	static final String MENU_KEYBINDING = "MENU";	//not a keybinding, but this is what the local hub will be expecting for menu actions
 	static final String CTRL_SPACE = "Ctrl+Space";
+	static final String F3 = "F3";
+	static final String ALT_SHIFT_R = "Alt+Shift+R";
 	
 	//IDS
 	static final String ID_CONTENT_ASSIST = "org.eclipse.ui.edit.text.contentAssist.proposals";	
 	static final String ID_OPEN_CALL_HIERARCHY = "org.eclipse.jdt.ui.edit.text.java.open.call.hierarchy";
-
+	static final String ID_OPEN_DECLARATION= "org.eclipse.jdt.ui.edit.text.java.open.editor";
+	static final String ID_RENAME_REFACTOR = "org.eclipse.jdt.ui.edit.text.java.rename.element";
+	
 	//NAMES
 	static final String NAME_CONTENT_ASSIST = "Content Assist";
 	static final String NAME_OPEN_CALL_HIERARCHY = "Open Call Hierarchy";
-
+	static final String NAME_OPEN_DECLARATION = "Open Declaration";
+	static final String NAME_RENAME_REFACTOR = "Rename - Refactoring";
+	
 	//misc
 	static final int DEFAULT_DURATION = 15000;
-	static final String KEYBINDING_DELTA = "keybinding";
-	static final String MENU_DELTA = "menu";
+	static final String DELTA_KEYBINDING = "keybinding";
+	static final String DELTA_MENU = "menu";
+	static final String DELTA_ACTIVATED = "activated";
 	
 	static InteractionEvent makeMenuCommandInteractionEvent(String informalCommandName, Date startDate, Date endDate) 
 	{
-		InteractionEvent ie = makeMockInteractionEvent(Kind.COMMAND, informalCommandName, MENU_DELTA, startDate, endDate);
+		InteractionEvent ie = makeMockInteractionEvent(Kind.COMMAND, informalCommandName, DELTA_MENU, startDate, endDate);
 		return ie;
 	}
 
 	static InteractionEvent makeKeyBoardCommandInteractionEvent(String commandId, Date startDate, Date endDate) 
 	{	
-		InteractionEvent ie = makeMockInteractionEvent(Kind.COMMAND, commandId, KEYBINDING_DELTA, startDate, endDate);
+		InteractionEvent ie = makeMockInteractionEvent(Kind.COMMAND, commandId, DELTA_KEYBINDING, startDate, endDate);
+		return ie;
+	}
+	
+	static InteractionEvent makeKeyBoardCommandInteractionEvent(String commandId, Date startAndEndDate) 
+	{	
+		InteractionEvent ie = makeMockInteractionEvent(Kind.COMMAND, commandId, DELTA_KEYBINDING, startAndEndDate, startAndEndDate);
 		return ie;
 	}
 
-	static InteractionEvent makeMockInteractionEvent(Kind kindOfCommand, String commandId, String deltaType, Date startDate, Date endDate) 
+	private static InteractionEvent makeMockInteractionEvent(Kind kindOfCommand, String commandId, String deltaType, Date startDate, Date endDate) 
 	{
 		//could be mock(InteractionEvent), but this is more "lifelike"
 		return new InteractionEvent(kindOfCommand, null, null, commandId, null, deltaType, 1.0f, startDate, endDate);
@@ -56,10 +69,17 @@ public class MockInteractionEventHandler
 		IBindingService service = mock(IBindingService.class);
 		addKeyBinding(service,ID_CONTENT_ASSIST,CTRL_SPACE);
 		addKeyBinding(service, ID_OPEN_CALL_HIERARCHY, MENU_KEYBINDING);
+		addKeyBinding(service, ID_OPEN_DECLARATION, F3);
+		addKeyBinding(service, ID_RENAME_REFACTOR, ALT_SHIFT_R);
 		return service;
 	}
 
-	static void addKeyBinding(IBindingService service, String commandId, String keyBinding) {
+	//This is common enough that it gets its own convenience method.
+	static InteractionEvent makeWorkbenchWindowEvent(Date startAndEndDate) {
+		return makeMockInteractionEvent(Kind.COMMAND, "org.eclipse.ui.internal.WorkbenchWindow", DELTA_ACTIVATED, startAndEndDate, startAndEndDate);
+	}
+
+	private static void addKeyBinding(IBindingService service, String commandId, String keyBinding) {
 		when(service.getBestActiveBindingFormattedFor(commandId)).thenReturn(keyBinding);
 	}
 
@@ -68,11 +88,12 @@ public class MockInteractionEventHandler
 		CommandNameServce testService = mock(CommandNameServce.class);
 		addCommandNamePair(testService, ID_CONTENT_ASSIST, NAME_CONTENT_ASSIST);
 		addCommandNamePair(testService, ID_OPEN_CALL_HIERARCHY, NAME_OPEN_CALL_HIERARCHY);
+		addCommandNamePair(testService, ID_OPEN_DECLARATION, NAME_OPEN_DECLARATION);
+		addCommandNamePair(testService, ID_RENAME_REFACTOR, NAME_RENAME_REFACTOR);
 		return testService;
 	}
 
-	static void addCommandNamePair(CommandNameServce service, String commandId, String commandName) {
+	private static void addCommandNamePair(CommandNameServce service, String commandId, String commandName) {
 		when(service.lookUpCommandName(commandId)).thenReturn(commandName);
 	}
-	
 }
--- src/edu/ncsu/lubick/toolmanagement/tests/TestInteractionEventConversion.java
+++ src/edu/ncsu/lubick/toolmanagement/tests/TestInteractionEventConversion.java
@@ -3,6 +3,8 @@ package edu.ncsu.lubick.toolmanagement.tests;
 import static edu.ncsu.lubick.toolmanagement.tests.MockInteractionEventHandler.*;
 import static org.junit.Assert.*;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -19,7 +21,8 @@ import edu.ncsu.lubick.util.KeyBindingDirectory;
 public class TestInteractionEventConversion 
 {
 
-	
+	private DateFormat humanDateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+
 	private InteractionEventConvertor converter;
 
 	@BeforeClass
@@ -28,7 +31,7 @@ public class TestInteractionEventConversion
 		KeyBindingDirectory.initializeBindingService(makeMockedKeyBindingService());
 		CommandNameDirectory.initializeCommandService(makeMockedCommandService());
 	}
-	
+
 	@Before
 	public void setUp()
 	{
@@ -44,61 +47,104 @@ public class TestInteractionEventConversion
 	@Test
 	public void testBasicKeystrokeConversion() throws Exception
 	{
-		InteractionEvent ie = makeKeyBoardCommandInteractionEvent(ID_CONTENT_ASSIST, new Date(), new Date());
-		
+		InteractionEvent ie = makeKeyBoardCommandInteractionEvent(ID_CONTENT_ASSIST, new Date());
+
 		converter.foundThisInteractionEvent(ie);
-		
+
 		List<ToolEvent> outputEvents = converter.getConvertedEvents();
-		
+
 		assertNotNull(outputEvents);
 		assertEquals(1, outputEvents.size());
-		
+
 		ToolEvent outputEvent = outputEvents.get(0);
 		assertEquals(CTRL_SPACE, outputEvent.getToolKeyPresses());
 		assertEquals(NAME_CONTENT_ASSIST, outputEvent.getToolName());
 		assertEquals(DEFAULT_DURATION, outputEvent.getDuration());
 
-		
+
 	}
-	
+
 	@Test
 	public void testBasicMenuConversion() throws Exception 
 	{//org.eclipse.jdt.ui.edit.text.java.open.call.hierarchy
-	 //Open Call Hierarchy
+		//Open Call Hierarchy
 		//Eclipse generates two events for menu operations : a menu one and then a keyboard one that matches what was done.
 		//This emulates that behavior
 
 		Date startAndEndDate = new Date();
 		InteractionEvent menuEvent = makeMenuCommandInteractionEvent(MENU_NAME_OPEN_CALL_HIERARCHY, startAndEndDate, startAndEndDate);
-		InteractionEvent correspondingKeyboardCommand = makeKeyBoardCommandInteractionEvent(ID_OPEN_CALL_HIERARCHY, startAndEndDate, startAndEndDate);
-		
+		InteractionEvent correspondingKeyboardCommand = makeKeyBoardCommandInteractionEvent(ID_OPEN_CALL_HIERARCHY, startAndEndDate);
+
 		converter.foundThisInteractionEvent(menuEvent);
-		
+
 		List<ToolEvent> outputEvents = converter.getConvertedEvents();
-		
+
 		assertNotNull(outputEvents);
 		assertEquals(0, outputEvents.size());
-		
+
 		converter.foundThisInteractionEvent(correspondingKeyboardCommand);
-		
+
 		outputEvents = converter.getConvertedEvents();
-		
+
 		assertNotNull(outputEvents);
 		assertEquals(1, outputEvents.size());
-		
+
 		ToolEvent outputEvent = outputEvents.get(0);
 		assertEquals(MENU_KEYBINDING, outputEvent.getToolKeyPresses());
 		assertEquals(NAME_OPEN_CALL_HIERARCHY, outputEvent.getToolName());
 		assertEquals(DEFAULT_DURATION, outputEvent.getDuration());
 
-		
+
 	}
-	
+
 	@Test
-	public void testKeystrokeConversionWithWindowNoise() throws Exception 
+	public void testDoubleKeystrokeConversionWithWindowNoise() throws Exception 
 	{
+		/*  This test emulates this course of action
+		 * 	[startdate: Wed Oct 16 21:12:47 EDT 2013, kind: command, sourceHandle: null, origin: org.eclipse.ui.internal.WorkbenchWindow,
+		 * 			delta: activated, endDate: Wed Oct 16 21:12:47 EDT 2013, navigation: null, interestContribution: 1.0, StructureKind: null, StructureHandle: null]
+			[startdate: Wed Oct 16 21:12:57 EDT 2013, kind: command, sourceHandle: null, origin: org.eclipse.jdt.ui.edit.text.java.open.editor,
+			 		delta: keybinding, endDate: Wed Oct 16 21:12:57 EDT 2013, navigation: null, interestContribution: 1.0, StructureKind: null, StructureHandle: null]
+			[startdate: Wed Oct 16 21:13:00 EDT 2013, kind: command, sourceHandle: null, origin: org.eclipse.jdt.ui.edit.text.java.rename.element,
+			 		delta: keybinding, endDate: Wed Oct 16 21:13:00 EDT 2013, navigation: null, interestContribution: 1.0, StructureKind: null, StructureHandle: null]
+			[startdate: Wed Oct 16 21:13:09 EDT 2013, kind: command, sourceHandle: null, origin: org.eclipse.ui.internal.WorkbenchWindow,
+			 		delta: activated, endDate: Wed Oct 16 21:13:09 EDT 2013, navigation: null, interestContribution: 1.0, StructureKind: null, StructureHandle: null]
+		 */
+		
+		Date firstDate = humanDateFormat.parse("Wed Oct 16 21:12:47 EDT 2013");
+		Date secondDate = humanDateFormat.parse("Wed Oct 16 21:12:57 EDT 2013");
+		Date thirdDate = humanDateFormat.parse("Wed Oct 16 21:13:00 EDT 2013");
+		Date fourthDate = humanDateFormat.parse("Wed Oct 16 21:13:09 EDT 2013");
+		
+		InteractionEvent firstWorkbenchWindowEvent = makeWorkbenchWindowEvent(firstDate);
+		InteractionEvent secondWorkbenchWindowEvent = makeWorkbenchWindowEvent(fourthDate);
+		
+		InteractionEvent openDeclarationEvent = makeKeyBoardCommandInteractionEvent(ID_OPEN_DECLARATION, secondDate);
+		InteractionEvent renameRefactorEvent = makeKeyBoardCommandInteractionEvent(ID_RENAME_REFACTOR, thirdDate);
+		
+		converter.foundThisInteractionEvent(firstWorkbenchWindowEvent);
+		converter.foundThisInteractionEvent(openDeclarationEvent);
+		converter.foundThisInteractionEvent(renameRefactorEvent);
+		converter.foundThisInteractionEvent(secondWorkbenchWindowEvent);
+		
+		
+		List<ToolEvent> outputEvents = converter.getConvertedEvents();
+
+		assertNotNull(outputEvents);
+		assertEquals(2, outputEvents.size());
+
+		ToolEvent outputEvent = outputEvents.get(0);
+		assertEquals(F3, outputEvent.getToolKeyPresses());
+		assertEquals(NAME_OPEN_DECLARATION, outputEvent.getToolName());
+		assertEquals(DEFAULT_DURATION, outputEvent.getDuration());
+		
+		outputEvent = outputEvents.get(1);
+		assertEquals(ALT_SHIFT_R, outputEvent.getToolKeyPresses());
+		assertEquals(NAME_RENAME_REFACTOR, outputEvent.getToolName());
+		assertEquals(DEFAULT_DURATION, outputEvent.getDuration());
 		
 	}
+
 
 
 
